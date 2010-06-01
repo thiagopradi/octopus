d742c856b088df6edc4cff4b5a0d437b2eca5bcf
Thiago Pradi <tchandy@gmail.com>
2010-06-01 15:48:24 -0300

    Specs for Octopus base module

--- spec/octopus/octopus_spec.rb
+++ spec/octopus/octopus_spec.rb
@@ -1,7 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Octopus do
-  it "should load the shards.yml file to start working" do
-    Octopus.config().should be_kind_of(Hash)
+  describe "#config method" do
+    it "should load shards.yml file to start working" do
+      Octopus.config().should be_kind_of(Hash)
+    end
+  end
+
+  describe "#directory method" do
+    it "should return the directory that contains the shards.yml file" do
+      Octopus.directory().should == File.expand_path(File.dirname(__FILE__) + '/../') 
+    end
   end
 end

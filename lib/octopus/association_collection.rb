b380a84b9d62dbb76200b57e614d982c990c38a7
Thiago Pradi <tchandy@gmail.com>
2010-06-21 15:09:50 -0300

    Refactoring

--- lib/octopus/association_collection.rb
+++ lib/octopus/association_collection.rb
@@ -1,4 +1,9 @@
 class ActiveRecord::Associations::AssociationCollection
+  
+  def should_wrap_the_connection?
+    @owner.current_shard != nil
+  end
+  
   def count(column_name = nil, options = {})
     if @reflection.options[:counter_sql]
       @reflection.klass.count_by_sql(@counter_sql)
@@ -12,10 +17,8 @@ class ActiveRecord::Associations::AssociationCollection
       end
 
       value = @reflection.klass.send(:with_scope, construct_scope) do 
-        if @owner.current_shard != nil
-          @owner.using @owner.current_shard do 
-            @reflection.klass.count(column_name, options) 
-          end
+        if should_wrap_the_connection?
+          @owner.using(@owner.current_shard) { @reflection.klass.count(column_name, options) } 
         else        
           @reflection.klass.count(column_name, options) 
         end
@@ -32,55 +35,11 @@ class ActiveRecord::Associations::AssociationCollection
     end
   end
 
-  def find(*args)
-    options = args.extract_options!
-    if @owner.current_shard != nil
-      @owner.using @owner.current_shard do 
-
-        # If using a custom finder_sql, scan the entire collection.
-        if @reflection.options[:finder_sql]
-          expects_array = args.first.kind_of?(Array)
-          ids           = args.flatten.compact.uniq.map { |arg| arg.to_i }
-
-          if ids.size == 1
-            id = ids.first
-            record = load_target.detect { |r| id == r.id }
-            expects_array ? [ record ] : record
-          else
-            load_target.select { |r| ids.include?(r.id) }
-          end
-        else
-          merge_options_from_reflection!(options)
-          construct_find_options!(options)
-
-          find_scope = construct_scope[:find].slice(:conditions, :order)
-
-          with_scope(:find => find_scope) do
-            relation = @reflection.klass.send(:construct_finder_arel, options, @reflection.klass.send(:current_scoped_methods))
-
-            case args.first
-            when :first, :last
-
-              relation.send(args.first)
-            when :all
-              records = relation.all
-              @reflection.options[:uniq] ? uniq(records) : records
-            else
-              relation.find(*args)            
-            end
-          end
-        end
-      end
-    end
-  end
-
 
   def delete(*records)
     remove_records(records) do |records, old_records|
-      if @owner.current_shard != nil
-        @owner.using @owner.current_shard do 
-          delete_records(old_records) if old_records.any?
-        end
+      if should_wrap_the_connection?
+        @owner.using(@owner.current_shard) { delete_records(old_records) if old_records.any? }
       else
         delete_records(old_records) if old_records.any?      
       end
@@ -95,18 +54,14 @@ class ActiveRecord::Associations::AssociationCollection
     return self if length.zero? # forces load_target if it hasn't happened already
 
     if @reflection.options[:dependent] && @reflection.options[:dependent] == :destroy
-      if @owner.current_shard != nil
-        @owner.using @owner.current_shard do 
-          destroy_all
-        end
+      if should_wrap_the_connection?
+        @owner.using(@owner.current_shard) { destroy_all } 
       else        
         destroy_all
       end
     else          
-      if @owner.current_shard != nil
-        @owner.using @owner.current_shard do 
-          delete_all
-        end
+      if should_wrap_the_connection?
+        @owner.using(@owner.current_shard) { delete_all } 
       else        
         delete_all
       end
@@ -121,7 +76,7 @@ class ActiveRecord::Associations::AssociationCollection
     else
       create_record(attrs) do |record|
         yield(record) if block_given?
-        record.current_shard = @owner.current_shard
+        record.current_shard = @owner.current_shard if should_wrap_the_connection?
         record.save
       end
     end
@@ -130,7 +85,7 @@ class ActiveRecord::Associations::AssociationCollection
   def create!(attrs = {})
     create_record(attrs) do |record|
       yield(record) if block_given?
-      record.current_shard = @owner.current_shard      
+      record.current_shard = @owner.current_shard if should_wrap_the_connection?
       record.save!
     end
   end
@@ -140,7 +95,7 @@ class ActiveRecord::Associations::AssociationCollection
       attributes.collect { |attr| build(attr, &block) }
     else
       build_record(attributes) do |record|
-        record.current_shard = @owner.current_shard
+        record.current_shard = @owner.current_shard if should_wrap_the_connection?
         block.call(record) if block_given?
         set_belongs_to_association_for(record)
       end

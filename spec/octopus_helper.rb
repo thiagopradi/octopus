716a73e9fc09d674766c77bc18297f09f06c1877
Thiago Pradi <tchandy@gmail.com>
2010-06-22 16:24:18 -0300

    Moving helper methods to octopus_helper

---
+++ spec/octopus_helper.rb
@@ -0,0 +1,31 @@
+def clean_all_shards()
+  ActiveRecord::Base.using(:master).connection.shards.keys.each do |shard_symbol|
+    ['schema_migrations', 'users', 'clients', 'cats', 'items', 'keyboards', 'computers', 'permissions_roles', 'roles', 'permissions', 'assignments', 'projects', 'programmers'].each do |tables|
+      ActiveRecord::Base.using(shard_symbol).connection.execute("DELETE FROM #{tables};") 
+    end
+  end
+end
+
+def clean_connection_proxy()
+  Thread.current[:connection_proxy] = nil  
+end
+
+def migrating_to_version(version, &block)
+  begin
+    ActiveRecord::Migrator.run(:up, MIGRATIONS_ROOT, version)
+    yield
+  ensure
+    ActiveRecord::Migrator.run(:down, MIGRATIONS_ROOT, version)
+  end
+end
+
+def using_enviroment(enviroment, &block)
+  begin
+    Octopus.instance_variable_set(:@env, enviroment.to_s)
+    clean_connection_proxy()
+    yield
+  ensure
+    Octopus.instance_variable_set(:@env, 'production')
+    clean_connection_proxy()
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require "spec/database_connection"
 require "action_pack"
 require "action_controller"
 require 'octopus'
+require "octopus_helper"
 
 Spec::Runner.configure do |config|  
   config.mock_with :rspec
@@ -17,37 +18,5 @@ Spec::Runner.configure do |config|
 
   config.after(:each) do
     clean_all_shards()
-  end
-end
-
-def clean_all_shards()
-  ActiveRecord::Base.using(:master).connection.shards.keys.each do |shard_symbol|
-    ['schema_migrations', 'users', 'clients', 'cats', 'items', 'keyboards', 'computers', 'permissions_roles', 'roles', 'permissions', 'assignments', 'projects', 'programmers'].each do |tables|
-      ActiveRecord::Base.using(shard_symbol).connection.execute("DELETE FROM #{tables};") 
-    end
-  end
-end
-
-def migrating_to_version(version, &block)
-  begin
-    ActiveRecord::Migrator.run(:up, MIGRATIONS_ROOT, version)
-    yield
-  ensure
-    ActiveRecord::Migrator.run(:down, MIGRATIONS_ROOT, version)
-  end
-end
-
-def clean_connection_proxy()
-  Thread.current[:connection_proxy] = nil  
-end
-
-def using_enviroment(enviroment, &block)
-  begin
-    Octopus.instance_variable_set(:@env, enviroment.to_s)
-    clean_connection_proxy()
-    yield
-  ensure
-    Octopus.instance_variable_set(:@env, 'production')
-    clean_connection_proxy()
   end
 end

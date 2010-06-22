9122d5fbb98f03a6127407320e88f7991b98d3a2
Thiago Pradi <tchandy@gmail.com>
2010-06-22 16:25:49 -0300

    Removing unnecessary import

--- spec/database_connection.rb
+++ spec/database_connection.rb
@@ -1,6 +1,5 @@
 require 'rubygems'
 require 'active_record'
-require 'logger'
 require "pg"
 
 ActiveRecord::Base.establish_connection(:adapter => "mysql", :database => "octopus_shard1", :username => "root", :password => "")

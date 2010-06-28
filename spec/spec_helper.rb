c0144960977f4fb44335ce038a2f98892deec699
Thiago Pradi <tchandy@gmail.com>
2010-06-28 09:25:22 -0300

    Removing code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,11 @@ MIGRATIONS_ROOT = File.expand_path(File.join(File.dirname(__FILE__),  'migration
 require 'spec'
 require 'spec/autorun'
 require "spec/database_connection"
-require "action_pack"
 require "action_controller"
 require 'octopus'
 require "octopus_helper"
 
 Spec::Runner.configure do |config|  
-  config.mock_with :rspec
-
   config.before(:each) do
     Octopus.stub!(:directory).and_return(File.dirname(__FILE__))
     require "database_models"

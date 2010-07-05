b940feb32a30cd8229ed2a982ce91795b9e94b83
Thiago Pradi <tchandy@gmail.com>
2010-07-05 14:59:49 -0300

    Initialize schema_migrations table inside using_group method

--- lib/octopus/migration.rb
+++ lib/octopus/migration.rb
@@ -22,7 +22,17 @@ module Octopus::Migration
 
   def using_group(*args)
     Octopus.config()
-    
+
+    args.each do |group_shard|
+      shards = self.connection().instance_variable_get(:@groups)[group_shard] || []
+
+      shards.each do |shard|
+        if !ActiveRecord::Base.using(shard).connection.table_exists?(ActiveRecord::Migrator.schema_migrations_table_name())
+          ActiveRecord::Base.using(shard).connection.initialize_schema_migrations_table 
+        end
+      end
+    end
+
     if args.size == 1
       self.connection().block = true
       self.connection().current_group = args.first

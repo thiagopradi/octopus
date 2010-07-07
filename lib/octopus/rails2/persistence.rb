f0c311dd3fdd5a2eb8bcedd4810d99c4776ab3f5
Thiago Pradi <tchandy@gmail.com>
2010-07-07 01:00:47 -0300

    Fixing issues

--- lib/octopus/rails2/persistence.rb
+++ lib/octopus/rails2/persistence.rb
@@ -1,6 +1,12 @@
 class ActiveRecord::Base
   def delete
-    self.using(self.current_shard) do
+    if should_set_current_shard?
+      self.using(self.current_shard) do
+        self.class.delete(id) unless new_record?
+        @destroyed = true
+        freeze
+      end
+    else
       self.class.delete(id) unless new_record?
       @destroyed = true
       freeze
@@ -8,22 +14,43 @@ class ActiveRecord::Base
   end
 
   def destroy
-    self.using(self.current_shard) do
-      unless new_record?
-        connection.delete(
-        "DELETE FROM #{self.class.quoted_table_name} " +
-        "WHERE #{connection.quote_column_name(self.class.primary_key)} = #{quoted_id}",
-        "#{self.class.name} Destroy"
-        )
+    if should_set_current_shard?
+      self.using(self.current_shard) do
+        unless new_record?
+          connection.delete(
+          "DELETE FROM #{self.class.quoted_table_name} " +
+          "WHERE #{connection.quote_column_name(self.class.primary_key)} = #{quoted_id}",
+          "#{self.class.name} Destroy"
+          )
+        end
       end
+      @destroyed = true
+      freeze
+    else
+      self.using(self.current_shard) do
+        unless new_record?
+          connection.delete(
+          "DELETE FROM #{self.class.quoted_table_name} " +
+          "WHERE #{connection.quote_column_name(self.class.primary_key)} = #{quoted_id}",
+          "#{self.class.name} Destroy"
+          )
+        end
+      end
+      @destroyed = true
+      freeze
     end
-    @destroyed = true
-    freeze
   end
 
   def reload(options = nil)
-    self.using(self.current_shard) do
-
+    if should_set_current_shard?
+      self.using(self.current_shard) do
+        clear_aggregation_cache
+        clear_association_cache
+        @attributes.update(self.class.send(:with_exclusive_scope) { self.class.find(self.id, options) }.instance_variable_get('@attributes'))
+        @attributes_cache = {}
+        self
+      end
+    else
       clear_aggregation_cache
       clear_association_cache
       @attributes.update(self.class.send(:with_exclusive_scope) { self.class.find(self.id, options) }.instance_variable_get('@attributes'))

d3f6cc2bf60d0a0ac30db03d65773d06b4015185
Thiago Pradi <tchandy@gmail.com>
2010-07-23 14:01:42 -0300

    Removing duplicated code

--- lib/octopus/has_and_belongs_to_many_association.rb
+++ lib/octopus/has_and_belongs_to_many_association.rb
@@ -4,11 +4,7 @@ module Octopus::HasAndBelongsToManyAssociation
       alias_method_chain :insert_record, :octopus
     end
   end
-
-  def should_wrap_the_connection?
-    @owner.respond_to?(:current_shard) && @owner.current_shard != nil
-  end
-
+  
   def insert_record_with_octopus(record, force = true, validate = true)
     if should_wrap_the_connection?
       Octopus.using(@owner.current_shard) { insert_record_without_octopus(record, force, validate) } 

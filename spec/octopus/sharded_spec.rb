3837d2b2499b8db7e6508d3160a5e79f783fe333
Thiago Pradi <tchandy@gmail.com>
2010-07-26 14:29:19 -0300

    specs for new sharding feature

--- spec/octopus/sharded_spec.rb
+++ spec/octopus/sharded_spec.rb
@@ -2,12 +2,31 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "when the database is replicated" do
   before(:each) do
+    Octopus.stub!(:env).and_return("not_entire_sharded")
+    clean_connection_proxy()
+  end
+ 
+  it "should not send all queries to the specified slave" do
+    User.create!(:name => "Thiago")
+
+    using_enviroment :not_entire_sharded do 
+      Octopus.using(:russia) do
+        User.create!(:name => "Thiago")
+      end
+    end
     
+    User.count.should == 2
   end
   
-  it "should not send all queries to the specified slave" do
-    Octopus.using(:russia) do
-    
+  it "should pick the shard based on current_shard when you have a sharded model" do
+    Cat.create!(:name => "Thiago")
+
+    using_enviroment :not_entire_sharded do 
+      Octopus.using(:russia) do
+        Cat.create!(:name => "Thiago")
+      end
     end
+    
+    Cat.count.should == 1
   end
 end

712a0516e53ae9c68975e6c5e0e0693043cb1e64
Thiago Pradi <tchandy@gmail.com>
2011-01-09 15:26:49 -0200

    Performance fix, thanks Mike Ragalie

--- lib/octopus/model.rb
+++ lib/octopus/model.rb
@@ -22,6 +22,8 @@ module Octopus::Model
     end
 
     def hijack_initializer()
+      return if respond_to?(:set_current_shard)
+      
       attr_accessor :current_shard
       after_initialize :set_current_shard
       before_save :reload_connection

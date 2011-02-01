225e80e79b484bb295f41164f800c648fdf55b36
Thiago Pradi <tchandy@gmail.com>
2011-01-31 23:30:32 -0200

    Cleaning all database files after the spec suite

--- sample_app/features/support/env.rb
+++ sample_app/features/support/env.rb
@@ -58,6 +58,13 @@ if defined?(ActiveRecord::Base)
   end
 end
 
+After do 
+  `rm #{Rails.root.to_s}/db/america.sqlite3`
+  `rm #{Rails.root.to_s}/db/asia.sqlite3`
+  `rm #{Rails.root.to_s}/db/development.sqlite3`
+  `rm #{Rails.root.to_s}/db/europe.sqlite3`
+end
+
 Before do 
   `rm #{Rails.root.to_s}/db/america.sqlite3`
   `rm #{Rails.root.to_s}/db/asia.sqlite3`

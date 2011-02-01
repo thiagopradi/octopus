33dd145d1efec61ee56cc36f4866ae2dd646d13e
Thiago Pradi <tchandy@gmail.com>
2011-01-31 23:31:01 -0200

    This code stinks... removing it

--- sample_app/features/step_definitions/seeds_steps.rb
+++ sample_app/features/step_definitions/seeds_steps.rb
@@ -1,6 +1,4 @@
 Then /^the "([^"]*)" shard should have one user named "([^"]*)"$/ do |shard_name, user_name|
-  # TODO - WTF?! - Why we need to instantiate a new proxy?
-  Thread.current[:connection_proxy] = nil
   User.using(shard_name.to_sym).where(:name => user_name).count.should == 1
 end
 

9e1f49bf16eec712e2c1448c09211acee049d5ef
Thiago Pradi <tchandy@gmail.com>
2011-03-12 17:55:35 -0300

    Small optimization on spec_helper

--- spec/octopus_helper.rb
+++ spec/octopus_helper.rb
@@ -1,7 +1,8 @@
 def clean_all_shards()
-  ActiveRecord::Base.using(:master).connection.instance_variable_get(:@shards).keys.each do |shard_symbol|
+  @@shards ||= ActiveRecord::Base.using(:master).connection.instance_variable_get(:@shards).keys
+  @@shards.each do |shard_symbol|
     ['schema_migrations', 'users', 'clients', 'cats', 'items', 'keyboards', 'computers', 'permissions_roles', 'roles', 'permissions', 'assignments', 'projects', 'programmers'].each do |tables|
-      ActiveRecord::Base.using(shard_symbol).connection.execute("DELETE FROM #{tables};") 
+      ActiveRecord::Base.using(shard_symbol).connection.execute("DELETE FROM #{tables}") 
     end
   end
 end

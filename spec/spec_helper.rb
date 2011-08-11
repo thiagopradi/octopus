a3fc69fda8c662d9f72faa47b7edec3011667b51
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2011-08-11 18:01:33 -0300

    Move database_connection, database_models and octopus_helper to spec/support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,18 @@
 require "rubygems"
-require File.expand_path(File.dirname(__FILE__)) + "/database_connection"
 require "bundler/setup"
 require "mysql2"
 require "active_record"
 require "action_controller"
 require "octopus"
-require "octopus_helper"
+require "support/database_connection"
+require "support/octopus_helper"
 
 MIGRATIONS_ROOT = File.expand_path(File.join(File.dirname(__FILE__),  'migrations'))
 
 RSpec.configure do |config|
   config.before(:each) do
     Octopus.stub!(:directory).and_return(File.dirname(__FILE__))
-    require "database_models"
+    require "support/database_models"
     clean_all_shards()
   end
 end
--- spec/support/database_connection.rb
+++ spec/support/database_connection.rb
@@ -1,4 +1,1 @@
-require 'rubygems'
-require 'active_record'
-
 ActiveRecord::Base.establish_connection(:adapter => "mysql2", :database => "octopus_shard1", :username => "root", :password => "")

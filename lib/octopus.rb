642cb1b7841397eef9d87fc4ea2b4f9b87568095
John McGrath <john@wordie.org>
2012-01-20 17:28:24 -0800

    Rails 3.2 support: use 'active_support/core_ext/class/attribute' instead of 'active_support/core_ext/class/inheritable_attributes' with 3.2

--- lib/octopus.rb
+++ lib/octopus.rb
@@ -1,4 +1,8 @@
-require 'active_support/core_ext/class/inheritable_attributes'
+if Gem.loaded_specs['activesupport'].version > Gem::Version.create('3.2.0.pre') # ActiveSupport 3.2
+  require 'active_support/core_ext/class/attribute'
+else # ActiveSupport 3.0, 3.1
+  require 'active_support/core_ext/class/inheritable_attributes'
+end
 require "yaml"
 require "erb"
 

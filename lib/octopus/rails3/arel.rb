952ba3b2fd649667d65f898f3d2ab8223e64603c
Josh Tabak <josh@justin.tv>
2012-06-28 16:15:17 -0700

    Avoid changing current_model to ActiveRecord::Base

--- lib/octopus/rails3/arel.rb
+++ lib/octopus/rails3/arel.rb
@@ -1,13 +1,13 @@
 class Arel::Visitors::ToSql
   def quote value, column = nil
-    ActiveRecord::Base.connection.quote value, column
+    ActiveRecord::Base.connection_proxy.quote value, column
   end
 
   def quote_table_name name
-    ActiveRecord::Base.connection.quote_table_name(name)
+    ActiveRecord::Base.connection_proxy.quote_table_name(name)
   end
 
   def quote_column_name name
-    Arel::Nodes::SqlLiteral === name ? name : ActiveRecord::Base.connection.quote_column_name(name)
+    Arel::Nodes::SqlLiteral === name ? name : ActiveRecord::Base.connection_proxy.quote_column_name(name)
   end
 end

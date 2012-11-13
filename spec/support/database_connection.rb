54bf49325194369750b7e4f66ef05e9806079b5a
Mike Ragalie <ragalie@verbasoftware.com>
2012-11-13 09:54:46 -0800

    Allow custom database config on ActiveRecord::Base
    This is necessary for many of the database-related rake tasks to work
    correctly.

--- spec/support/database_connection.rb
+++ spec/support/database_connection.rb
@@ -1,1 +1,2 @@
 ActiveRecord::Base.establish_connection(:adapter => "mysql", :database => "octopus_shard_1", :username => "root", :password => "")
+ActiveRecord::Base.custom_octopus_connection = false

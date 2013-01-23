551ebcd8c79479e536957a83916b5e92e40ba473
Mike Ragalie <ragalie@verbasoftware.com>
2013-01-23 13:15:51 -0800

    Fix specs when railties isn't available
    When we drop 2.3 support we can just depend on railties, but for the
    moment we just need to recover when we don't have it.

--- lib/octopus/railtie.rb
+++ lib/octopus/railtie.rb
@@ -1,9 +1,13 @@
-require "rails/railtie"
+begin
+  require "rails/railtie"
 
-module Octopus
-  class Railtie < Rails::Railtie
-    rake_tasks do
-      Dir[File.join(File.dirname(__FILE__), "../tasks/*.rake")].each { |ext| load ext }
+  module Octopus
+    class Railtie < Rails::Railtie
+      rake_tasks do
+        Dir[File.join(File.dirname(__FILE__), "../tasks/*.rake")].each { |ext| load ext }
+      end
     end
   end
+rescue LoadError
+  # nop
 end

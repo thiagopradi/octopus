1566601fb6d60aa8d0a8341cdf7d12c2f4b23e22
Mike Ragalie <ragalie@verbasoftware.com>
2013-01-23 12:03:22 -0800

    Expose rake tasks to Rails and update README for 2.3 support

---
+++ lib/octopus/railtie.rb
@@ -0,0 +1,9 @@
+require "rails/railtie"
+
+module Octopus
+  class Railtie < Rails::Railtie
+    rake_tasks do
+      Dir[File.join(File.dirname(__FILE__), "../tasks/*.rake")].each { |ext| load ext }
+    end
+  end
+end

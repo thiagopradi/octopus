1f84e14ff79cea06d28031b807e3c05a02fcbb00
Thiago Pradi <thiago.pradi@gmail.com>
2013-01-27 22:48:14 -0200

    any? spec should only create the record when executed

--- spec/octopus/model_spec.rb
+++ spec/octopus/model_spec.rb
@@ -299,10 +299,10 @@ describe Octopus::Model do
       User.using(:brazil).find(:all, :conditions => {:name => "User2"}).count.should == 1
     end
 
-    describe "any?" do
-      before { User.using(:brazil).create!(:name => "User1") }
+    if Octopus.rails3?
+      describe "any?" do
+        before { User.using(:brazil).create!(:name => "User1") }
 
-      if Octopus.rails3?
         it "works when true" do
           scope = User.using(:brazil).where(:name => "User1")
           scope.any?.should be_true

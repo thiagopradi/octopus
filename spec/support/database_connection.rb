0634369d7425169d71edc9dee62bc1edb5795c08
Thiago Pradi <thiago.pradi@gmail.com>
2013-02-03 23:50:21 -0200

    Adding ActiveRecord logger

--- spec/support/database_connection.rb
+++ spec/support/database_connection.rb
@@ -1,2 +1,3 @@
 ActiveRecord::Base.establish_connection(:adapter => "mysql", :database => "octopus_shard_1", :username => "root", :password => "")
 ActiveRecord::Base.custom_octopus_connection = false
+ActiveRecord::Base.logger = Logger.new(File.open('database.log', 'a'))

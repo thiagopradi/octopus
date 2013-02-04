7062035c80564e78ad82d9629a1ac2c67ab2f6ab
Thiago Pradi <thiago.pradi@gmail.com>
2013-02-04 01:16:46 -0200

    Adding deprecation warning for Octopus::Logger

--- lib/octopus/logger.rb
+++ lib/octopus/logger.rb
@@ -1,6 +1,11 @@
 require "logger"
 
 class Octopus::Logger < Logger
+  def initialize(logdev, shift_age = 0, shift_size = 1048576)
+    ActiveSupport::Deprecation.warn "Octopus::Logger is deprecated and will be removed in Octopus 0.6.x", caller
+    super
+  end
+
   def format_message(severity, timestamp, progname, msg)
     str = super
 
--- spec/octopus/logger_spec.rb
+++ spec/octopus/logger_spec.rb
@@ -1,20 +1,28 @@
 require "spec_helper"
 
-describe Octopus::Logger, :shards => [] do
-
-	before :each do
-		@out = StringIO.new
-		@log = Octopus::Logger.new(@out)
-		ActiveRecord::Base.logger = @log
-	end
-
-	after :each do
-		ActiveRecord::Base.logger = nil
+describe Octopus::Logger, :shards => [:canada] do
+  before :each do
+    @out = StringIO.new
+    @log = Octopus::Logger.new(@out)
+    ActiveRecord::Base.logger = @log
   end
 
-	it "should add to the default logger what shard the query was sent" do
-    pending("Logger needs some Love")
-    #User.using(:canada).create!(:name => "test")
-		#@out.string.should =~ /Shard: canada/
-	end
+  after :each do
+    ActiveRecord::Base.logger = nil
+  end
+
+  if Octopus.rails3?
+    it "should add to the default logger what shard the query was sent" do
+      User.using(:canada).create!(:name => "test")
+      @out.string.should =~ /Shard: canada/
+    end
+  end
+
+  it "should be deprecated" do
+    @last_message = nil
+    ActiveSupport::Deprecation.behavior = Proc.new { |message| @last_message = message }
+    @log = Octopus::Logger.new(@out)
+
+    @last_message.should =~ /DEPRECATION WARNING: Octopus::Logger is deprecated and will be removed in Octopus 0\.6\.x\./
+  end
 end

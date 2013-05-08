442574039f222b02cd53b790ad37ddc59b92dd89
Thiago Pradi <thiago.pradi@gmail.com>
2013-05-08 00:37:40 -0300

    Fixing spec suite after the removal of Logger

--- spec/support/database_connection.rb
+++ spec/support/database_connection.rb
@@ -1,2 +1,4 @@
+require "logger"
+
 ActiveRecord::Base.establish_connection(:adapter => "mysql2", :database => "octopus_shard_1", :username => "root", :password => "")
 ActiveRecord::Base.logger = Logger.new(File.open('database.log', 'a'))

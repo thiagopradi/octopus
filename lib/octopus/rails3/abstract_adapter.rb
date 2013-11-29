7363650a351903cac1dfec9fb3e6e96ab16b5eea
Nick Marden <nick@marden.org>
2013-11-29 15:54:06 -0500

    Make shard-friendly clear_query_cache implementation less convoluted

--- lib/octopus/rails3/abstract_adapter.rb
+++ lib/octopus/rails3/abstract_adapter.rb
@@ -26,7 +26,6 @@ module Octopus
 
       def self.included(base)
         base.alias_method_chain :initialize, :octopus_shard
-        base.alias_method_chain :clear_query_cache, :octopus
       end
 
       def octopus_shard
@@ -36,16 +35,6 @@ module Octopus
       def initialize_with_octopus_shard(*args)
         initialize_without_octopus_shard(*args)
         @instrumenter = InstrumenterDecorator.new(self, @instrumenter)
-      end
-
-      # Intercept calls to clear_query_cache and make sure that all
-      # query caches on all shards are invalidated, just to be safe.
-      def clear_query_cache_with_octopus
-        if Octopus.enabled?
-          ActiveRecord::Base.connection_proxy.clear_all_query_caches!
-        else
-          clear_query_cache_without_octopus
-        end
       end
 
     end

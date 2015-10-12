154bf0c26f3a083b2e9f8958b96d5e3b2909b755
Nick Marden <nick@marden.org>
2015-10-12 18:53:39 +0800

    Association#target_scope results -> Octopus::RelationProxy via run_on_shard
    It seems that the "double-select" issues that were addressed by the
    introduction of Octopus::AssociationShardTracking::QueryOnCurrentShard
    were simpler to handle by simply guaranteeing that, whenever a call to
    AR::Association#target_scope returns an AR::Relation, it is cast into
    an Octopus::RelationProxy so that future calls against that relation are
    guaranteed to point to the correct shard.

--- lib/octopus/association.rb
+++ lib/octopus/association.rb
@@ -2,6 +2,7 @@ module Octopus
   module Association
     def self.included(base)
       base.send(:include, Octopus::ShardTracking::Dynamic)
+      base.sharded_methods :target_scope
     end
 
     def current_shard

d12e748356a299feae81e7361dd1229b0c322bd3
Thiago Pradi <thiago.pradi@gmail.com>
2016-12-07 22:48:58 -0200

    Come on travis

--- spec/support/database_connection.rb
+++ spec/support/database_connection.rb
@@ -1,4 +1,4 @@
 require 'logger'
 
 ActiveRecord::Base.establish_connection(:adapter => 'mysql2', :database => 'octopus_shard_1', :username => 'root', :password => '')
-ActiveRecord::Base.logger = Logger.new(STDOUT)
+ActiveRecord::Base.logger = Logger.new(File.open('database.log', 'a'))

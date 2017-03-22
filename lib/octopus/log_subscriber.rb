a83bfbc82e382eb671e2ab1b7826f2c5116f98a9
Thiago Pradi <thiago.pradi@gmail.com>
2017-03-21 22:29:47 -0300

    Use #prepend instead of alias method chain for LogSubscriber

--- lib/octopus/log_subscriber.rb
+++ lib/octopus/log_subscriber.rb
@@ -1,26 +1,18 @@
 # Implementation courtesy of db-charmer.
 module Octopus
   module LogSubscriber
-    def self.included(base)
-      base.send(:attr_accessor, :octopus_shard)
+    attr_accessor :octopus_shard
 
-      base.send :alias_method, :sql_without_octopus_shard, :sql
-      base.send :alias_method, :sql, :sql_with_octopus_shard
-
-      base.send :alias_method, :debug_without_octopus_shard, :debug
-      base.send :alias_method, :debug, :debug_with_octopus_shard
-    end
-
-    def sql_with_octopus_shard(event)
+    def sql(event)
       self.octopus_shard = event.payload[:octopus_shard]
-      sql_without_octopus_shard(event)
+      super
     end
 
-    def debug_with_octopus_shard(msg)
+    def debug(msg)
       conn = octopus_shard ? color("[Shard: #{octopus_shard}]", ActiveSupport::LogSubscriber::GREEN, true) : ''
-      debug_without_octopus_shard(conn + msg)
+      super(conn + msg)
     end
   end
 end
 
-ActiveRecord::LogSubscriber.send(:include, Octopus::LogSubscriber)
+ActiveRecord::LogSubscriber.send(:prepend, Octopus::LogSubscriber)

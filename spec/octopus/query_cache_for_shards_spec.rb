49482adc72cc7438a58cc0d04f70705f70b1e895
Matthew Hirst <hirst.mat@gmail.com>
2018-06-29 13:04:34 +0200

    Add test that fails when QueryCacheForShards is run without enabled check.

--- spec/octopus/query_cache_for_shards_spec.rb
+++ spec/octopus/query_cache_for_shards_spec.rb
@@ -4,14 +4,37 @@ unless Octopus.rails4?
   describe Octopus::ConnectionPool::QueryCacheForShards do
     subject(:query_cache_on_shard) { ActiveRecord::Base.using(:brazil).connection.query_cache_enabled }
 
-    context 'when query cache is enabled on the primary connection_pool' do
-      before { ActiveRecord::Base.connection_pool.enable_query_cache! }
-      it { is_expected.to be true }
+    context 'Octopus enabled' do
+      context 'when query cache is enabled on the primary connection_pool' do
+        before { ActiveRecord::Base.connection_pool.enable_query_cache! }
+        it { is_expected.to be true }
+      end
+
+      context 'when query cache is disabled on the primary connection_pool' do
+        before { ActiveRecord::Base.connection_pool.disable_query_cache! }
+        it { is_expected.to be false }
+      end
     end
 
-    context 'when query cache is disabled on the primary connection_pool' do
-      before { ActiveRecord::Base.connection_pool.disable_query_cache! }
-      it { is_expected.to be false }
+    context 'Octopus disabled' do
+      before do
+        Rails = double
+        allow(Rails).to receive(:env).and_return('staging')
+      end
+
+      after do
+        Object.send(:remove_const, :Rails)
+      end
+
+      context 'when query cache is enabled on the primary connection_pool' do
+        before { ActiveRecord::Base.connection_pool.enable_query_cache! }
+        it { is_expected.to be true }
+      end
+
+      context 'when query cache is disabled on the primary connection_pool' do
+        before { ActiveRecord::Base.connection_pool.disable_query_cache! }
+        it { is_expected.to be false }
+      end
     end
   end
 end

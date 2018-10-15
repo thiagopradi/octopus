906a4f1f8ed57f399a8337f3982107f10a59f533
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2018-10-15 19:13:39 -0400

    only migrate specified shards.

--- lib/octopus/migration.rb
+++ lib/octopus/migration.rb
@@ -109,8 +109,8 @@ module Octopus
 
       def migrate_with_octopus(&block)
         return migrate_without_octopus(&block) unless connection.is_a?(Octopus::Proxy)
-
-        connection.send_queries_to_multiple_shards(connection.shard_names) do
+        shards = migrations.map(&:shards).flatten.map(&:to_s)
+        connection.send_queries_to_multiple_shards(shards) do
           migrate_without_octopus(&block)
         end
         rescue ActiveRecord::UnknownMigrationVersionError => e

a96f52300958e61ae3ac7beb79514fe6a7a9f8d8
Cal Heldenbrand <cal@fbsdata.com>
2019-07-03 10:00:51 -0500

    Create 4 total dbs for shard/replication testing

--- docker/db2/create_databases.sh
+++ docker/db2/create_databases.sh
@@ -2,5 +2,8 @@
 set -e
 source /home/db2inst1/sqllib/db2profile
 db2start
-echo "Creating databases, this may take around 6 - 8 minutes..."
-time db2 create database octopus6
+echo "Creating databases, this may take around 6 - 8 minutes per database..."
+for db in octopus6 octopus7 octopus8 octopus9; do
+  echo "create databse $db"
+  time db2 create database $db
+done

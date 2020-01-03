1050bb44d17c3bceeee6275d6f5aeefb78900de0
Pete Brumm <pete@kindful.com>
2020-01-03 15:32:13 -0600

    fixing by doing a similar reset per request that rails does
    this does introduce a dependency on RequestStore gem to work.
    but it also falls back

--- lib/octopus/proxy.rb
+++ lib/octopus/proxy.rb
@@ -72,12 +72,22 @@ module Octopus
       connection_pool.automatic_reconnect ||= true
       con = nil
       if !connection_pool.connected?
-        con = shards[Octopus.master_shard].connection
-        if con.query_cache_enabled
+        master_con = shards[Octopus.master_shard].connection
+        if master_con.query_cache_enabled
+          con ||= connection_pool.connection
           con.enable_query_cache!
         end
       end
       con ||= connection_pool.connection
+      if defined?(RequestStore)
+        # we need to verify! the connection before use.   Rails does this by default but octopus bypasses this mechanism.
+        # the primary connection works fine but if you use using(x) it doesn't checkout the connection from the pool correctly
+        # now we verify each pool once per request
+        if RequestStore.store["octopus.verify_pool_#{connection_pool.object_id}"].blank?
+          RequestStore.store["octopus.verify_pool_#{connection_pool.object_id}"] = 1
+          con.verify!
+        end
+      end
       con
     end
 

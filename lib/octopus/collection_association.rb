da5d280388875b5346bcaeede02abe708265d308
Tyler Swayne <swaynetyler@gmail.com>
2020-09-26 13:41:19 -0700

    Rails 6 support

--- lib/octopus/collection_association.rb
+++ lib/octopus/collection_association.rb
@@ -7,8 +7,7 @@ module Octopus
                              :load_target, :reload, :size, :select
       else
         base.sharded_methods :reader, :writer, :ids_reader, :ids_writer, :create, :create!,
-                             :build, :any?, :count, :empty?, :first, :include?, :last, :length,
-                             :load_target, :many?, :reload, :size, :select, :uniq
+                             :build, :empty?, :include?, :load_target, :reload, :size, :select
       end
     end
   end

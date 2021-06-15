6cf144b7c194036707d5ae56c1f1af0abad975be
Jacob Sheehy <jacobjsheehy@gmail.com>
2021-06-15 16:58:13 -0700

    [chore] Fix typo and bump version

--- lib/octopus/collection_association.rb
+++ lib/octopus/collection_association.rb
@@ -1,7 +1,7 @@
 module Octopus
   module CollectionAssociation
     def self.included(base)
-      if Octopus.atlreast_rails51?
+      if Octopus.atleast_rails51?
         base.sharded_methods :reader, :writer, :ids_reader, :ids_writer, :create, :create!,
                              :build, :include?,
                              :load_target, :reload, :size, :select

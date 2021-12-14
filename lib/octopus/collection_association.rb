0ed5055555545d8bda7a5432c6f749c8f36d5fc8
Kevin Bruccoleri <kevinbruccoleri@gmail.com>
2021-12-14 17:30:02 -0500

    Monkey-patch Octopus::CollectionAssocation

--- lib/octopus/collection_association.rb
+++ lib/octopus/collection_association.rb
@@ -1,7 +1,9 @@
 module Octopus
   module CollectionAssociation
     def self.included(base)
-      if Octopus.rails51? || Octopus.rails52?
+      # https://github.com/thiagopradi/octopus/issues/540
+      # https://github.com/schovi/octopus/commit/a39612969286891c705384c214d7f1d0c365609f
+      if Octopus.atleast_rails51?
         base.sharded_methods :reader, :writer, :ids_reader, :ids_writer, :create, :create!,
                              :build, :include?,
                              :load_target, :reload, :size, :select

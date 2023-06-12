ed94fce1b8957b3ea6b09ea54e2eb676a888d2b9
Daniel Cunha <daniel.cunha@nuuvem.com>
2023-06-12 09:16:07 -0300

    Rails 6 dummy support

--- lib/octopus/collection_association.rb
+++ lib/octopus/collection_association.rb
@@ -1,7 +1,7 @@
 module Octopus
   module CollectionAssociation
     def self.included(base)
-      if Octopus.rails51? || Octopus.rails52?
+      if Octopus.atleast_rails51?
         base.sharded_methods :reader, :writer, :ids_reader, :ids_writer, :create, :create!,
                              :build, :include?,
                              :load_target, :reload, :size, :select
